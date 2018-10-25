962b2fa20c2f5d419de767edc397090e38b82959
Muammarbanana <muammarfanani2@gmail.com>
2018-10-26 00:05:34 +0700

    Selesai no 1

---
+++ Modul3/app/src/main/java/com/example/ahmad/modul3app/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.ahmad.modul3app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private static final String TAG = "MainActivity" ;
+    private String mSpinnerLabel = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.label_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner.
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        mSpinnerLabel = adapterView.getItemAtPosition(i).toString();
+        showText(view);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        Log.d(TAG, "onNothingSelected: ");
+    }
+
+    public void showText(View view) {
+        EditText editText = (EditText) findViewById(R.id.edittext);
+        if (editText != null) {
+            // Assign to showString both the entered string and mSpinnerLabel.
+            String showString = (editText.getText().toString() + " - " + mSpinnerLabel);
+            // Assign to phoneNumberResult the view for text_phonelabel to prepare to show it.
+            TextView phoneNumberResult = (TextView) findViewById(R.id.tekx);
+            // Show the showString in the phoneNumberResult.
+            if (phoneNumberResult != null) phoneNumberResult.setText(showString);
+        }
+    }
+}
